f0dbf8822c927d7aa872d8f68a9523e9cd450fe1
Jonas Inhani Martins <jonasmweb@hotmail.com>
2023-09-30 16:12:37 -0300

    fix: all tests run

--- src/nestjs/jest.config.ts
+++ src/nestjs/jest.config.ts
@@ -1,24 +1,64 @@
+const fc_micro_videos_path =
+  '<rootDir>/../../../node_modules/@fc/micro-videos/dist';
+
 export default {
   displayName: {
     name: 'nestjs',
-    color: 'magentaBright'
+    color: 'magentaBright',
   },
-  moduleFileExtensions: [
-    "js",
-    "json",
-    "ts"
-  ],
-  rootDir: "src",
-  testRegex: ".*\\.spec\\.ts$",
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\..*spec\\.ts$',
   transform: {
-    "^.+\\.(t|j)s$": "@swc/jest"
+    '^.+\\.(t|j)s$': '@swc/jest',
   },
-  collectCoverageFrom: [
-    "**/*.(t|j)s"
-  ],
-  coverageDirectory: "../coverage",
-  testEnvironment: "node",
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageProvider: 'v8',
+  coverageDirectory: '../__coverage',
+  testEnvironment: 'node',
   moduleNameMapper: {
-    '@fc/micro\\-videos/(.*)$': '<rootDir>/../../../node_modules/@fc/micro-videos/dist/$1'
-  }
-}
+    '@fc/micro\\-videos/(.*)$': `${fc_micro_videos_path}/$1`,
+    //'#seedwork/domain': '<rootDir>/../../../node_modules/@fc/micro-videos/dist/@seedwork/domain/index.js',
+    //TODO - vamos ver depois
+    '#seedwork/(.*)$': `${fc_micro_videos_path}/@seedwork/$1`,
+    //'#category/domain': '<rootDir>/../../../node_modules/@fc/micro-videos/dist/category/domain/index.js',
+    //vamos ver mais tarde se é necessário
+    //TODO - vamos ver depois
+    '#category/(.*)$': `${fc_micro_videos_path}/category/$1`,
+    '#cast-member/(.*)$': `${fc_micro_videos_path}/cast-member/$1`,
+  },
+  setupFilesAfterEnv: ['../../@core/src/@seedwork/domain/tests/jest.ts'],
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+    },
+  },
+};
+
+// export default {
+//   displayName: {
+//     name: 'nestjs',
+//     color: 'magentaBright'
+//   },
+//   moduleFileExtensions: [
+//     "js",
+//     "json",
+//     "ts"
+//   ],
+//   rootDir: "src",
+//   testRegex: ".*\\.spec\\.ts$",
+//   transform: {
+//     "^.+\\.(t|j)s$": "@swc/jest"
+//   },
+//   collectCoverageFrom: [
+//     "**/*.(t|j)s"
+//   ],
+//   coverageDirectory: "../coverage",
+//   testEnvironment: "node",
+//   moduleNameMapper: {
+//     '@fc/micro\\-videos/(.*)$': '<rootDir>/../../../node_modules/@fc/micro-videos/dist/$1'
+//   }
+// }
